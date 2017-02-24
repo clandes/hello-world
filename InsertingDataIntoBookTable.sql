7b66927a2b3231625d0744faf497596a12536d6a
clandes <clandes@users.noreply.github.com>
2017-02-23 19:26:30 -0800

    Add files via upload

---
+++ InsertingDataIntoBookTable.sql
@@ -0,0 +1,31 @@
+INSERT INTO SmallCityLibrary.dbo.book
+
+(BookId, Title, PublisherName)
+
+VALUES
+
+(1, 'The Lost Tribe', 'Picador USA'),
+(2, 'The Shining', 'Anchor Books'),
+(3, 'Carrie', 'Anchor Books'),
+(4, 'Last of the Breed', 'Bantam'),
+(5, 'The Rider of the Ruby Hills', 'Bantam'),
+(6, 'The Strong Shall Live', 'Bantam'),
+(7, 'The Walking Drum', 'Bantam'),
+(8, 'Sacketts Land', 'Bantam'),
+(9, 'The Cat Who Played Brahms', 'Berkley Publishing'),
+(10, 'The Cat Who Played Post Office', 'Berkley Publishing'),
+(11, 'The Cat Who Saw Red', 'Berkley Publishing'),
+(12, 'The Cat Who Ate Danish Modern', 'Berkley Publishing'),
+(13, 'The Cat Who Smelled Smoke', 'Berkley Publishing'),
+(14, 'Beneath the Skin', 'Signet'),
+(15, 'In the Blood', 'Signet'),
+(16, 'Past Redemption', 'Signet'),
+(17, 'Beyond the Pale', 'Signet'),
+(18, 'Under Darkness', 'Signet'),
+(19, 'Chihuawolf', 'Sourcebooks'),
+(20, 'Careful What You Wish For', 'Signet'),
+(21, 'Vineyard Deceit', 'Avon Books'),
+(22, 'Death in Vineyard Waters', 'Avon Books'),
+(23, 'Growing Up Again', 'St. Martins Press'),
+(24, 'Doris [Day]', 'Bantam'),
+(25, 'Edisto Jinx', 'Bell Bridge Books')
