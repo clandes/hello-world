21bab9f1d364486232072baee231d63224dc508c
clandes <clandes@users.noreply.github.com>
2017-07-25 19:34:48 -0700

    Add files via upload

---
+++ FileMovingDrill/Python course Item 63.py
@@ -0,0 +1,6 @@
+import shutil
+shutil.move('C:\\Users\\Cheryl\\Desktop\\FolderA\\bears.txt', 'C:\\Users\\Cheryl\\FolderB\\')
+shutil.move('C:\\Users\\Cheryl\\Desktop\\FolderA\\frogs.txt', 'C:\\Users\\Cheryl\\FolderB\\')
+shutil.move('C:\\Users\\Cheryl\\Desktop\\FolderA\\dogs.txt', 'C:\\Users\\Cheryl\\FolderB\\')
+shutil.move('C:\\Users\\Cheryl\\Desktop\\FolderA\\cats.txt', 'C:\\Users\\Cheryl\\FolderB\\')
+
