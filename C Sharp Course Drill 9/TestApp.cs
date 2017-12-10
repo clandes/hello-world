ce7e7ce12c1807345c6eb6a00bf97ee2f1fc0961
clandes <clandes@users.noreply.github.com>
2017-12-09 16:22:18 -0800

    Add files via upload

---
+++ C Sharp Course Drill 9/TestApp.cs
@@ -0,0 +1,20 @@
+// A simple C# application.
+using System;
+
+// Don't need this anymore.
+// using System.Windows.Forms;
+
+class TestApp
+{
+    public static void Main()
+    {
+        Console.WriteLine("Testing! 1, 2, 3");
+
+        // Don't need this anymore, either.
+        // MessageBox.Show("Hello...");
+
+        // Exercise the HelloMessage class!
+        HelloMessage h = new HelloMessage();
+        h.Speak();
+    }
+}
